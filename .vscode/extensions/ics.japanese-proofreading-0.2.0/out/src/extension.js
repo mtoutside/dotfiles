d4d665d6a66a07c262320f3407aee72bbc72ac5b
mtoutside <more.humor.or@gmail.com>
2019-01-07 11:41:38 +0900

    changes vscode extension

---
+++ .vscode/extensions/ics.japanese-proofreading-0.2.0/out/src/extension.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const path = require("path");
+const vscode_1 = require("vscode");
+const vscode_languageclient_1 = require("vscode-languageclient");
+let client;
+function activate(context) {
+    const serverModule = context.asAbsolutePath(path.join("out", "server.js"));
+    const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"] };
+    const serverOptions = {
+        run: { module: serverModule, transport: vscode_languageclient_1.TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: vscode_languageclient_1.TransportKind.ipc,
+            options: debugOptions,
+        },
+    };
+    const clientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [
+            { scheme: "file", language: "plaintext" },
+            { scheme: "file", language: "markdown" },
+        ],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contain in the workspace
+            configurationSection: 'japaneseProofreading',
+            fileEvents: vscode_1.workspace.createFileSystemWatcher("**/.clientrc"),
+        },
+    };
+    client = new vscode_languageclient_1.LanguageClient("japaneseProofreading", "japaneseProofreading", serverOptions, clientOptions);
+    client.registerProposedFeatures();
+    client.start();
+}
+exports.activate = activate;
+function deactivate() {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
+exports.deactivate = deactivate;
